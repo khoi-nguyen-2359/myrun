b4235e8007c76e4da3fc47101c7ea66a5eb0502f
Khoi Nguyen <akhoi90@gmail.com>
2020-09-15 15:03:09 +0700

    Initial commit

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,42 @@
+object Versions {
+    const val versionName = "0.1.0" // x.y.zz
+    const val versionCode = 100  // 1_xyzz
+
+    const val compileSdk = 30
+    const val targetSdk = 30
+    const val minSdk = 21
+
+    const val androidGradlePlugin = "4.0.1"
+    const val googleServices = "4.3.3"
+    const val kotlin = "1.4.10"
+    const val crashlyticsGradlePlugin = "2.2.1"
+    const val playServicesGradlePlugin = "4.3.3"
+
+    const val appCompat = "1.2.0"
+    const val constraintLayout = "2.0.1"
+
+    const val dagger = "2.28.3"
+    const val room = "2.2.5"
+    const val lifecycle = "2.2.0"
+    const val timber = "4.7.1"
+    const val facebook = "[5,6)"
+    const val multidex = "1.0.3"
+
+    const val coroutinesCore = "1.3.8"
+    const val coroutinesAndroid = "1.3.7"
+    const val coroutinesPlayServices = "1.3.2"
+
+    const val playServicesMap = "17.0.0"
+    const val playServicesLocation = "17.0.0"
+    const val playServicesFitness = "19.0.0"
+    const val playServicesAuth = "18.1.0"
+
+    const val androidMapUtils = "1.1.0"
+
+    const val firebaseCore = "17.5.0"
+    const val firebaseFirestore = "21.6.0"
+    const val firebaseAuth = "19.4.0"
+    const val firebaseAnalytics = "17.5.0"
+    const val firebaseCrashlytics = "17.2.1"
+    const val crashlytics = "2.10.1"
+}
