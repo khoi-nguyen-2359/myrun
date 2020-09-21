0603b92c7c063e43782fa0edc42f2668d54fc3b5
Khoi Nguyen <akhoi90@gmail.com>
2020-09-21 12:37:12 +0700

    Remove trackme legacy

--- app/src/main/java/akio/apps/myrun/feature/_base/permissions/CheckRequiredPermissionsDelegate.kt
+++ app/src/main/java/akio/apps/myrun/feature/_base/permissions/CheckRequiredPermissionsDelegate.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 
 class CheckRequiredPermissionsDelegate(
@@ -23,23 +24,11 @@ class CheckRequiredPermissionsDelegate(
             return
         }
 
-        PermissionUtils.requestPermissions(activity, permissions, rcPermissions, {
-            AlertDialog.Builder(activity)
-                .setMessage(R.string.error_permission_settings_disabled)
-                .setPositiveButton(R.string.action_open_settings) { _, _ ->
-                    Intent().apply {
-                        action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
-                        data = Uri.parse("package:${activity.packageName}");
-                        activity.startActivity(this)
-                    }
-                }
-                .setNegativeButton(R.string.action_cancel) { _, _ ->
-                    activity.finish()
-                }
-                .setCancelable(false)
-                .create()
-                .show()
-        }, fragment)
+        if (fragment != null) {
+            fragment.requestPermissions(permissions, rcPermissions)
+        } else {
+            ActivityCompat.requestPermissions(activity, permissions, rcPermissions)
+        }
     }
 
     fun verifyPermissionsResult() {
