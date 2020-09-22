2d3a38d3971a16859b5483fb296c6f3860bdf16d
Khoi Nguyen <akhoi90@gmail.com>
2020-09-22 15:00:04 +0700

    Adjust styles for list in user timeline screen

--- app/src/main/java/akio/apps/myrun/feature/usertimeline/impl/ActivityPagingAdapter.kt
+++ app/src/main/java/akio/apps/myrun/feature/usertimeline/impl/ActivityPagingAdapter.kt
@@ -1,5 +1,7 @@
 package akio.apps.myrun.feature.usertimeline.impl
 
+import akio.apps.myrun.R
+import akio.apps.myrun.data.activity.ActivityType
 import akio.apps.myrun.databinding.ItemUserTimelineActivityBinding
 import akio.apps.myrun.feature.usertimeline.Activity
 import android.view.LayoutInflater
@@ -7,11 +9,24 @@ import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import kotlinx.coroutines.Dispatchers
+import java.text.SimpleDateFormat
 
 class ActivityPagingAdapter : PagingDataAdapter<Activity, ActivityViewHolder>(ACTIVITY_COMPARATOR, Dispatchers.Main, Dispatchers.Default) {
 
+    private val activityNameMap: Map<ActivityType, Int> = mapOf(
+        ActivityType.Running to R.string.activity_name_running,
+        ActivityType.Cycling to R.string.activity_name_cycling
+    )
+    private val timeFormatter: SimpleDateFormat = SimpleDateFormat("h:mm a")
+    private val dateFormatter: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivityViewHolder {
-        return ActivityViewHolder(ItemUserTimelineActivityBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return ActivityViewHolder(
+            ItemUserTimelineActivityBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            timeFormatter,
+            dateFormatter,
+            activityNameMap
+        )
     }
 
     override fun onBindViewHolder(holder: ActivityViewHolder, position: Int) {
--- app/src/main/java/akio/apps/myrun/feature/usertimeline/impl/ActivityViewHolder.kt
+++ app/src/main/java/akio/apps/myrun/feature/usertimeline/impl/ActivityViewHolder.kt
@@ -1,25 +1,50 @@
 package akio.apps.myrun.feature.usertimeline.impl
 
+import akio.apps._base.ui.getString
 import akio.apps.myrun.R
 import akio.apps.myrun.data.activity.ActivityType
 import akio.apps.myrun.databinding.ItemUserTimelineActivityBinding
 import akio.apps.myrun.feature.routetracking.model.RouteTrackingStats
 import akio.apps.myrun.feature.usertimeline.Activity
+import androidx.annotation.StringRes
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class ActivityViewHolder(
-    private val viewBinding: ItemUserTimelineActivityBinding
+    private val viewBinding: ItemUserTimelineActivityBinding,
+    private val timeFormatter: SimpleDateFormat,
+    private val dateFormatter: SimpleDateFormat,
+    private val activityTypeNameMap: Map<ActivityType, Int>
 ) : RecyclerView.ViewHolder(viewBinding.root) {
 
-    fun bind(activity: Activity) {
+    fun bind(activity: Activity) = viewBinding.apply {
         Glide.with(itemView)
             .load(activity.routeImage)
             .placeholder(R.drawable.ic_run_circle)
-            .into(viewBinding.runningActivityImage)
+            .into(runningActivityImage)
 
         val speed = activity.distance / activity.duration
-        viewBinding.runningActivityStatsView.setActivityType(activity.activityType)
-        viewBinding.runningActivityStatsView.update(RouteTrackingStats(activity.distance, speed, activity.duration))
+
+        activityStatsView.setActivityType(activity.activityType)
+        activityStatsView.update(RouteTrackingStats(activity.distance, speed, activity.duration))
+
+        val calendar = Calendar.getInstance()
+        val todayDate = TimeUnit.MILLISECONDS.toDays(calendar.timeInMillis)
+        val activityDate = TimeUnit.MILLISECONDS.toDays(activity.startTime)
+        when {
+            activityDate == todayDate -> activityTimeTextView.text = getString(R.string.item_activity_time_today, timeFormatter.format(Date(activity.startTime)))
+            todayDate - activityDate == 1L -> activityTimeTextView.text = getString(R.string.item_activity_time_yesterday, timeFormatter.format(Date(activity.startTime)))
+            else -> activityTimeTextView.text = "${dateFormatter.format(Date(activity.startTime))} ${timeFormatter.format(Date(activity.startTime))}"
+        }
+
+        calendar.timeInMillis = activity.startTime
+        when (calendar.get(Calendar.HOUR_OF_DAY)) {
+            in 5..11 -> activityTitleTextView.text = getString(R.string.item_activity_title_morning, getString(activityTypeNameMap[activity.activityType] ?: 0))
+            in 12..16 -> activityTitleTextView.text = getString(R.string.item_activity_title_afternoon, getString(activityTypeNameMap[activity.activityType] ?: 0))
+            in 17..24, in 0..4 -> activityTitleTextView.text = getString(R.string.item_activity_title_evening, getString(activityTypeNameMap[activity.activityType] ?: 0))
+        }
     }
 }
