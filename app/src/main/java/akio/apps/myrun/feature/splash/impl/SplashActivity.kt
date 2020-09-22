c3b1696993c571c5a94c0a481ca174e46ffbf37b
Khoi Nguyen <akhoi90@gmail.com>
2020-09-22 16:31:50 +0700

    Catch and show error in splash screen

--- app/src/main/java/akio/apps/myrun/feature/splash/impl/SplashActivity.kt
+++ app/src/main/java/akio/apps/myrun/feature/splash/impl/SplashActivity.kt
@@ -2,7 +2,9 @@ package akio.apps.myrun.feature.splash.impl
 
 import akio.apps._base.di.BaseInjectionActivity
 import akio.apps._base.lifecycle.observe
+import akio.apps._base.lifecycle.observeEvent
 import akio.apps.myrun.R
+import akio.apps.myrun.feature._base.utils.ActivityDialogDelegate
 import akio.apps.myrun.feature.usertimeline.impl.UserTimelineActivity
 import akio.apps.myrun.feature.routetracking.impl.RouteTrackingActivity
 import akio.apps.myrun.feature.splash.SplashViewModel
@@ -12,9 +14,13 @@ class SplashActivity: BaseInjectionActivity() {
 
     private val splashViewModel: SplashViewModel by lazy { getViewModel() }
 
+    private val dialogDelegate by lazy { ActivityDialogDelegate(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        observeEvent(splashViewModel.error, dialogDelegate::showExceptionAlert)
 
         observe(splashViewModel.isRouteTrackingInProgress) { isTracking ->
             finish()
