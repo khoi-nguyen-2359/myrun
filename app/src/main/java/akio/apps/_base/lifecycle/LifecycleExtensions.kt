28cca661d6ce49447075cbbf8465cbfbad8ef15f
Khoi Nguyen <akhoi90@gmail.com>
2021-01-23 00:24:21 +0700

    Add a small extracted method

--- app/src/main/java/akio/apps/_base/lifecycle/LifecycleExtensions.kt
+++ app/src/main/java/akio/apps/_base/lifecycle/LifecycleExtensions.kt
@@ -1,9 +1,12 @@
 package akio.apps._base.lifecycle
 
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.coroutineScope
 import androidx.lifecycle.observe
+import kotlinx.coroutines.CoroutineScope
 
 fun <T> LifecycleOwner.observe(liveData: LiveData<T>, block: (T) -> Unit) {
     liveData.observe(this, block)
@@ -16,3 +19,6 @@ fun <T> LifecycleOwner.observe(liveData: LiveData<T>, observer: Observer<T>) {
 fun <T> LifecycleOwner.observeEvent(liveData: LiveData<Event<T>>, block: (T) -> Unit) {
     liveData.observe(this, EventObserver { eventData -> block(eventData) })
 }
+
+val Fragment.viewLifecycleScope: CoroutineScope
+    get() = viewLifecycleOwner.lifecycle.coroutineScope
