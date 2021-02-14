e325065acc2dc6302c2bcdea1d3b3124bcb43c6d
khoi-nguyen <khoi.nguyen@linecorp.com>
2021-02-14 12:17:34 +0700

    Use AS Canary version for Jetpack compose

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -6,49 +6,50 @@ object Versions {
     const val targetSdk = 30
     const val minSdk = 21
 
-    const val androidGradlePlugin = "4.1.2"
-    const val googleServices = "4.3.3"
+    const val agp = "7.0.0-alpha06"
     const val kotlin = "1.4.30"
-    const val crashlyticsGradlePlugin = "2.3.0"
-    const val playServicesGradlePlugin = "4.3.4"
+    const val coroutines = "1.4.2"
 
     const val appCompat = "1.2.0"
-    const val activity = "1.1.0"
-    const val fragment = "1.2.5"
-    const val constraintLayout = "2.0.2"
-    const val androidMaterial = "1.2.1"
-    const val workManager = "2.4.0"
+    const val activity = "1.2.0"
+    const val fragment = "1.3.0"
+    const val constraintLayout = "2.0.4"
+    const val androidMaterial = "1.3.0"
+    const val compose = "1.0.0-alpha12"
+
+    const val workManager = "2.5.0"
     const val paging = "3.0.0-alpha06"
-    const val room = "2.2.5"
-    const val lifecycle = "2.2.0"
+    const val room = "2.2.6"
+    const val lifecycle = "2.3.0"
+    const val lifecycleExtension = "2.2.0"
     const val multidex = "1.0.3"
 
     const val dagger = "2.28.3"
     const val timber = "4.7.1"
     const val glide = "4.11.0"
     const val facebookSdk = "[5,6)"
-    const val okhttp = "4.7.1"
+    const val okhttp = "4.8.1"
     const val gson = "2.8.6"
     const val retrofit = "2.9.0"
     const val tcxzPot = "0.2.0"
 
-    const val coroutines = "1.3.9"
-
+    const val playServicesGradlePlugin = "4.3.4"
     const val playServicesMap = "17.0.0"
     const val playServicesLocation = "17.1.0"
-    const val playServicesAuth = "18.1.0"
+    const val playServicesAuth = "19.0.0"
     const val playServicesFitness = "20.0.0"
 
     const val places = "2.4.0"
     const val androidMapUtils = "1.1.0"
 
     const val firebaseBom = "25.12.0"
+    const val crashlyticsGradlePlugin = "2.3.0"
 
     const val dataStore = "1.0.0-alpha01"
 
     // testing
     const val junit = "4.13.1"
-    const val coroutinesTest = "1.3.8"
+    const val coroutinesTest = "1.4.1"
     const val androidXCoreTesting = "2.1.0"
     const val mockitoCore = "3.6.28"
     const val mockitoKotlin = "2.2.0"
