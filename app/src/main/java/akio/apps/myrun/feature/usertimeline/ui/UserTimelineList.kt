23efaa626c1f41cb94308d0f44ec01332c35a0b6
Khoi Nguyen <akhoi90@gmail.com>
2021-05-28 23:08:14 +0700

    Fix ktlint error

--- app/src/main/java/akio/apps/myrun/feature/usertimeline/ui/UserTimelineList.kt
+++ app/src/main/java/akio/apps/myrun/feature/usertimeline/ui/UserTimelineList.kt
@@ -7,7 +7,6 @@ import akio.apps.myrun.feature.usertimeline.UserTimelineViewModel
 import akio.apps.myrun.feature.usertimeline.model.Activity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -33,7 +32,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
