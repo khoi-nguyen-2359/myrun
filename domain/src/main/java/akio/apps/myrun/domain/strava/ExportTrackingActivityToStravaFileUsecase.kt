073e7563f219c02718604fa78a99ad8af7a939ff
Khoi Nguyen <akhoi90@gmail.com>
2021-05-29 23:10:38 +0700

    Split class for writing activity to tcx file

--- domain/src/main/java/akio/apps/myrun/domain/strava/ExportTrackingActivityToStravaFileUsecase.kt
+++ domain/src/main/java/akio/apps/myrun/domain/strava/ExportTrackingActivityToStravaFileUsecase.kt
@@ -1,8 +1,5 @@
 package akio.apps.myrun.domain.strava
 
-import akio.apps.myrun._base.runfile.ClosableFileSerializer
-import akio.apps.myrun._base.runfile.ZipFileSerializer
-import akio.apps.myrun._base.utils.toGmsLatLng
 import akio.apps.myrun._di.NamedIoDispatcher
 import akio.apps.myrun.data.activity.ActivityRepository
 import akio.apps.myrun.data.activity.model.ActivityModel
@@ -13,32 +10,20 @@ import akio.apps.myrun.data.activityexport.model.ActivityLocation
 import akio.apps.myrun.data.activityexport.model.FileTarget
 import akio.apps.myrun.data.fitness.FitnessDataRepository
 import akio.apps.myrun.data.fitness.SingleDataPoint
+import akio.apps.myrun.domain.activityexport.ActivityTcxFileWriter
 import akio.apps.myrun.domain.routetracking.SaveRouteTrackingActivityUsecase
-import com.google.maps.android.SphericalUtil
-import com.sweetzpot.tcxzpot.Activities
-import com.sweetzpot.tcxzpot.Cadence
-import com.sweetzpot.tcxzpot.Intensity
-import com.sweetzpot.tcxzpot.Position
-import com.sweetzpot.tcxzpot.Sport
-import com.sweetzpot.tcxzpot.TCXDate
-import com.sweetzpot.tcxzpot.Track
-import com.sweetzpot.tcxzpot.TriggerMethod
-import com.sweetzpot.tcxzpot.builders.ActivityBuilder
-import com.sweetzpot.tcxzpot.builders.LapBuilder
-import com.sweetzpot.tcxzpot.builders.TrackpointBuilder
-import com.sweetzpot.tcxzpot.builders.TrainingCenterDatabaseBuilder
+import java.io.File
+import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
-import java.io.File
-import java.util.Date
-import javax.inject.Inject
 
 class ExportTrackingActivityToStravaFileUsecase @Inject constructor(
     private val fitnessDataRepository: FitnessDataRepository,
     private val activityFileTrackingRepository: ActivityFileTrackingRepository,
     private val exportActivityLocationRepository: ExportActivityLocationRepository,
     private val activityRepository: ActivityRepository,
+    private val activityTcxFileWriter: ActivityTcxFileWriter,
     @NamedIoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
     suspend operator fun invoke(
@@ -67,7 +52,7 @@ class ExportTrackingActivityToStravaFileUsecase @Inject constructor(
         )
 
         val outputFile = activityFileTrackingRepository.createEmptyFile(activity.id)
-        writeTrainingFile(
+        activityTcxFileWriter.writeTcxFile(
             activity,
             exportActivityLocations.await(),
             flattenCadences,
@@ -140,80 +125,5 @@ class ExportTrackingActivityToStravaFileUsecase @Inject constructor(
             }
         }
         return flattenCadences
-    }
-
-    private fun writeTrainingFile(
-        activity: ActivityModel,
-        locations: List<ActivityLocation>,
-        cadences: MutableList<Int>,
-        outputFile: File,
-        zip: Boolean
-    ) {
-        val serializer = if (zip)
-            ZipFileSerializer(outputFile, ".tcx")
-        else
-            ClosableFileSerializer(outputFile)
-
-        val avgCadence: Int? = if (cadences.isEmpty())
-            null
-        else
-            cadences.average().toInt()
-
-        val startDate = Date(activity.startTime)
-        val sportType = when (activity.activityType) {
-            ActivityType.Running -> Sport.RUNNING
-            else -> Sport.BIKING
-        }
-        var lastLocation: ActivityLocation? = locations.firstOrNull()
-        var currentDistance = 0.0
-        TrainingCenterDatabaseBuilder.trainingCenterDatabase()
-            .withActivities(
-                Activities.activities(
-                    ActivityBuilder.activity(sportType)
-                        .withID(TCXDate(startDate))
-                        .withLaps(
-                            LapBuilder.aLap(TCXDate(startDate))
-                                .withTotalTime(activity.duration.toDouble())
-                                .withDistance(activity.distance)
-                                .withIntensity(Intensity.ACTIVE)
-                                .withTriggerMethod(TriggerMethod.MANUAL)
-                                .withCalories(0)
-                                .apply {
-                                    avgCadence?.let { withCadence(Cadence.cadence(it)) }
-                                }
-                                .withTracks(
-                                    Track.trackWith(
-                                        locations.mapIndexed { index, waypoint ->
-                                            TrackpointBuilder.aTrackpoint()
-                                                .onTime(TCXDate(Date(waypoint.time)))
-                                                .withPosition(
-                                                    Position.position(
-                                                        waypoint.latitude,
-                                                        waypoint.longitude
-                                                    )
-                                                )
-                                                .withAltitude(waypoint.altitude)
-                                                .withCadence(Cadence.cadence(cadences[index]))
-                                                .apply {
-                                                    lastLocation?.let {
-                                                        currentDistance +=
-                                                            SphericalUtil.computeDistanceBetween(
-                                                                it.toGmsLatLng(),
-                                                                waypoint.toGmsLatLng()
-                                                            )
-                                                        lastLocation = waypoint
-                                                        withDistance(currentDistance)
-                                                    }
-                                                }
-                                                .build()
-                                        }
-                                    )
-                                )
-                        )
-                )
-            )
-            .build()
-            .serialize(serializer)
-        serializer.close()
     }
 }
