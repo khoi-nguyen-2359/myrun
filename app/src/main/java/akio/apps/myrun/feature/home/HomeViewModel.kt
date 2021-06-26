e850f53a95858445ef00cc377ddbf580182f9d83
Khoi Nguyen <akhoi90@gmail.com>
2021-06-26 19:16:51 +0700

    Add location accumulation option to configurator screen

--- app/src/main/java/akio/apps/myrun/feature/home/HomeViewModel.kt
+++ app/src/main/java/akio/apps/myrun/feature/home/HomeViewModel.kt
@@ -4,10 +4,11 @@ import akio.apps.myrun.data.routetracking.RouteTrackingConfiguration
 import akio.apps.myrun.data.routetracking.model.LocationRequestConfig
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.first
 
 class HomeViewModel @Inject constructor(
     private val routeTrackingConfiguration: RouteTrackingConfiguration
 ) : ViewModel() {
     suspend fun getLocationRequest(): LocationRequestConfig =
-        routeTrackingConfiguration.getLocationRequestConfig()
+        routeTrackingConfiguration.getLocationRequestConfig().first()
 }
--- configurator/src/main/java/akio/apps/myrun/configurator/RouteTrackingConfigurationViewModel.kt
+++ configurator/src/main/java/akio/apps/myrun/configurator/RouteTrackingConfigurationViewModel.kt
@@ -5,30 +5,65 @@ import akio.apps.myrun.data.routetracking.model.LocationRequestConfig
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import javax.inject.Inject
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class RouteTrackingConfigurationViewModel @Inject constructor(
     private val routeTrackingConfiguration: RouteTrackingConfiguration
 ) : ViewModel() {
 
-    private val locationRequestConfigMutableState: MutableStateFlow<LocationRequestConfig> =
-        MutableStateFlow(LocationRequestConfig.foo())
+    // This value is for rolling back when text input is not a number
+    private var cacheLocationRequestConfig: LocationRequestConfig = LocationRequestConfig.foo()
+    private var locationUpdateConfigState: MutableStateFlow<LocationUpdateConfiguration> =
+        MutableStateFlow(LocationUpdateConfiguration())
+    val locationUpdateConfigFlow: Flow<LocationUpdateConfiguration> = locationUpdateConfigState
 
     init {
-        initData()
-    }
-
-    suspend fun getLocationRequestConfig(): LocationRequestConfig =
         routeTrackingConfiguration.getLocationRequestConfig()
-
-    private fun initData() = viewModelScope.launch {
-        locationRequestConfigMutableState.value =
-            routeTrackingConfiguration.getLocationRequestConfig()
+            .onEach { cacheLocationRequestConfig = it }
+            .combine(routeTrackingConfiguration.isLocationAccumulationEnabled()) {
+                locationRequestConfig, isLocationAccumulationEnabled ->
+                LocationUpdateConfiguration(
+                    updateInterval = locationRequestConfig.updateInterval.toString(),
+                    fastestUpdateInterval = locationRequestConfig.fastestUpdateInterval.toString(),
+                    smallestDisplacement = locationRequestConfig.smallestDisplacement.toString(),
+                    isAccumulationEnabled = isLocationAccumulationEnabled
+                )
+            }
+            .onEach { locationUpdateConfigState.value = it }
+            .launchIn(viewModelScope)
     }
 
-    fun setLocationRequestInfo(requestConfig: LocationRequestConfig) = viewModelScope.launch {
-        routeTrackingConfiguration.setLocationRequestInfo(requestConfig)
-        initData()
+    fun onLocationUpdateConfigurationChanged(
+        updateConfig: LocationUpdateConfiguration
+    ) = viewModelScope.launch {
+        locationUpdateConfigState.value = updateConfig
     }
+
+    fun applyChanges() = viewModelScope.launch {
+        val updateConfig = locationUpdateConfigState.value
+        val updateLocationRequest = LocationRequestConfig(
+            updateInterval = updateConfig.updateInterval.toLongOrNull()
+                ?: cacheLocationRequestConfig.updateInterval,
+            fastestUpdateInterval = updateConfig.fastestUpdateInterval.toLongOrNull()
+                ?: cacheLocationRequestConfig.fastestUpdateInterval,
+            smallestDisplacement = updateConfig.smallestDisplacement.toFloatOrNull()
+                ?: cacheLocationRequestConfig.smallestDisplacement
+        )
+        routeTrackingConfiguration.setLocationRequestConfiguration(updateLocationRequest)
+        routeTrackingConfiguration.setLocationAccumulationEnabled(
+            updateConfig.isAccumulationEnabled
+        )
+    }
+
+    data class LocationUpdateConfiguration(
+        val updateInterval: String = "",
+        val fastestUpdateInterval: String = "",
+        val smallestDisplacement: String = "",
+        val isAccumulationEnabled: Boolean = false
+    )
 }
