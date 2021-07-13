d71b46391b5ea5c28a214da56e93b90da6d17d26
Khoi Nguyen <akhoi90@gmail.com>
2021-07-13 23:45:25 +0700

    [MR-67] Optimize activity item rendering

--- app/src/main/java/akio/apps/myrun/feature/activitydetail/ui/ActivityRouteImage.kt
+++ app/src/main/java/akio/apps/myrun/feature/activitydetail/ui/ActivityRouteImage.kt
@@ -8,8 +8,14 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import coil.request.ImageRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
 import com.google.accompanist.coil.rememberCoilPainter
+import com.google.accompanist.glide.rememberGlidePainter
 import com.google.accompanist.imageloading.ImageLoadState
 import com.google.accompanist.placeholder.PlaceholderHighlight
 import com.google.accompanist.placeholder.material.placeholder
@@ -20,8 +26,10 @@ fun ActivityRouteImage(
     activity: Activity,
     onClickAction: (() -> Unit)? = null
 ) {
+    val context = LocalContext.current
     val imagePainter = rememberCoilPainter(
         request = activity.routeImage,
+        requestBuilder = { size -> ImageRequest.Builder(context).size(size.width, size.height) },
         shouldRefetchOnSizeChange = { _, _ -> false },
         previewPlaceholder = R.drawable.ic_run_circle,
         fadeIn = true,
@@ -35,7 +43,7 @@ fun ActivityRouteImage(
             .aspectRatio(1.5f)
             .placeholder(
                 visible = imagePainter.loadState !is ImageLoadState.Success,
-                highlight = PlaceholderHighlight.shimmer()
+                color = Color(0xff8bb8f5)
             )
             .run {
                 if (onClickAction != null) {
