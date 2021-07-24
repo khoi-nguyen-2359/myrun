a4390b16efd37bb3e35fc9e27f46fae28a794854
Khoi Nguyen <akhoi90@gmail.com>
2021-07-24 18:44:48 +0700

    [MR-72] Fix unit tests

--- domain/src/test/java/akio/apps/myrun/domain/user/GetUserProfileUsecaseTest.kt
+++ domain/src/test/java/akio/apps/myrun/domain/user/GetUserProfileUsecaseTest.kt
@@ -1,36 +1,32 @@
 package akio.apps.myrun.domain.user
 
 import akio.apps._base.Resource
-import akio.apps._base.error.UnauthorizedUserError
 import akio.apps.myrun.data.authentication.UserAuthenticationState
 import akio.apps.myrun.data.authentication.model.UserAccount
 import akio.apps.myrun.data.userprofile.UserProfileRepository
 import akio.apps.myrun.data.userprofile.model.Gender
 import akio.apps.myrun.data.userprofile.model.UserProfile
+import com.nhaarman.mockitokotlin2.mock
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.never
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Test
-import org.mockito.ArgumentMatchers.anyString
-import org.mockito.Mock
-import org.mockito.Mockito.never
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
 import org.mockito.Mockito.`when` as whenever
 
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 class GetUserProfileUsecaseTest {
 
-    @Mock
-    lateinit var userProfileRepository: UserProfileRepository
-
-    @Mock
-    lateinit var userAuthenticationState: UserAuthenticationState
+    private lateinit var mockedUserProfileRepository: UserProfileRepository
+    private lateinit var mockedUserAuthenticationState: UserAuthenticationState
 
     private val defaultUserId = "defaultUserId"
     private val defaultEmail = "defaultEmail"
@@ -42,29 +38,31 @@ class GetUserProfileUsecaseTest {
 
     @Before
     fun setup() {
+        mockedUserProfileRepository = mock()
+        mockedUserAuthenticationState = mock()
         MockitoAnnotations.openMocks(this)
-        testee = GetUserProfileUsecase(userProfileRepository, userAuthenticationState)
+        testee = GetUserProfileUsecase(mockedUserProfileRepository, mockedUserAuthenticationState)
     }
 
     @Test
     fun `given user logged in, when get user profile, then return user profile data`() {
         // given
         val userProfile = createUserProfile()
-        whenever(userAuthenticationState.getUserAccountId()).thenReturn(defaultUserId)
-        whenever(userProfileRepository.getUserProfileFlow(defaultUserId)).thenReturn(
+        whenever(mockedUserAuthenticationState.requireUserAccountId()).thenReturn(defaultUserId)
+        whenever(mockedUserProfileRepository.getUserProfileFlow(defaultUserId)).thenReturn(
             flowOf(Resource.Success(userProfile))
         )
 
         // when
         runBlockingTest {
-            val userProfileFlow = testee.getUserProfileFlow(params.userId)
+            val userProfileFlow = testee.getUserProfileFlow(null)
             userProfileFlow.collect { userProfileResource ->
                 assertEquals(userProfile, userProfileResource.data)
             }
         }
 
-        verify(userAuthenticationState).getUserAccountId()
-        verify(userProfileRepository).getUserProfileFlow(defaultUserId)
+        verify(mockedUserAuthenticationState).requireUserAccountId()
+        verify(mockedUserProfileRepository).getUserProfileFlow(defaultUserId)
     }
 
     private fun createUserAccount(): UserAccount {
@@ -77,17 +75,19 @@ class GetUserProfileUsecaseTest {
         )
     }
 
-    @Test(expected = UnauthorizedUserError::class)
+    @Test(expected = IllegalStateException::class)
     fun `given user not logged in, when get user profile, then exception thrown`() {
         // given
-        whenever(userAuthenticationState.getUserAccountId()).thenReturn(null)
+        whenever(mockedUserAuthenticationState.requireUserAccountId()).thenThrow(
+            IllegalStateException()
+        )
 
         // when
-        testee.getUserProfileFlow(params.userId)
+        testee.getUserProfileFlow(null)
 
         // then
-        verify(userAuthenticationState).getUserAccountId()
-        verify(userProfileRepository, never()).getUserProfileFlow(anyString())
+        verify(mockedUserAuthenticationState).getUserAccountId()
+        verify(mockedUserProfileRepository, never()).getUserProfileFlow(anyString())
     }
 
     private fun createUserProfile(): UserProfile {
