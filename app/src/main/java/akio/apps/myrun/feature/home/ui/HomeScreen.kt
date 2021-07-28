40357e1d94b7527a0a333c19743dde29aedd234c
Khoi Nguyen <akhoi90@gmail.com>
2021-07-28 22:03:53 +0700

    [MR-77] Enable edge-2-edge in home screen

--- app/src/main/java/akio/apps/myrun/feature/home/ui/HomeScreen.kt
+++ app/src/main/java/akio/apps/myrun/feature/home/ui/HomeScreen.kt
@@ -3,40 +3,55 @@ package akio.apps.myrun.feature.home.ui
 import akio.apps.myrun.feature.usertimeline.UserTimelineViewModel
 import akio.apps.myrun.feature.usertimeline.model.Activity
 import akio.apps.myrun.feature.usertimeline.ui.UserTimeline
+import akio.apps.myrun.ui.theme.AppColors
 import akio.apps.myrun.ui.theme.AppTheme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomAppBar
-import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
-private val BottomAppBarHeight = 56.dp
+private val AppBarHeight = 56.dp
+private val FabSize = 56.dp
 
 @Composable
 fun HomeScreen(
     userTimelineViewModel: UserTimelineViewModel,
+    contentPaddings: PaddingValues,
     onClickUserProfileButton: () -> Unit,
     onClickFloatingActionButton: () -> Unit,
     onClickActivityItemAction: (Activity) -> Unit,
@@ -44,26 +59,69 @@ fun HomeScreen(
     onClickUserAvatar: (String) -> Unit
 ) {
     AppTheme {
-        Scaffold(
-            bottomBar = { HomeBottomBar(onClickUserProfileButton) },
-            floatingActionButton = { HomeFloatingActionButton(onClickFloatingActionButton) },
-            floatingActionButtonPosition = FabPosition.Center,
-            isFloatingActionButtonDocked = true
+        val topBarHeightDp = AppBarHeight + contentPaddings.calculateTopPadding()
+        val topBarHeightPx = with(LocalDensity.current) { topBarHeightDp.roundToPx().toFloat() }
+        val fabBoxHeightDp = FabSize * 4 / 3 + contentPaddings.calculateBottomPadding()
+        val fabBoxSizePx = with(LocalDensity.current) { fabBoxHeightDp.roundToPx().toFloat() }
+        var topBarOffsetY by remember { mutableStateOf(0f) }
+        var fabOffsetY by remember { mutableStateOf(0f) }
+        val nestedScrollConnection = remember {
+            object : NestedScrollConnection {
+                override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                    val delta = available.y
+                    var newOffset = topBarOffsetY + delta
+                    topBarOffsetY = newOffset.coerceIn(-topBarHeightPx, 0f)
+                    newOffset = fabOffsetY - delta
+                    fabOffsetY = newOffset.coerceIn(0f, fabBoxSizePx)
+                    return Offset.Zero
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(nestedScrollConnection)
         ) {
-            Box {
-                UserTimeline(
-                    userTimelineViewModel,
-                    PaddingValues(
-                        bottom = BottomAppBarHeight * 1.5f // avoid the bottom bar
-                    ),
-                    onClickActivityItemAction,
-                    onClickActivityFileAction,
-                    onClickUserAvatar
-                )
+            UserTimeline(
+                userTimelineViewModel,
+                PaddingValues(
+                    top = topBarHeightDp,
+                    bottom = fabBoxHeightDp // avoid the bottom bar
+                ),
+                onClickActivityItemAction,
+                onClickActivityFileAction,
+                onClickUserAvatar
+            )
+            HomeTopBar(
+                onClickUserProfileButton,
+                Modifier
+                    .height(topBarHeightDp)
+                    .align(Alignment.TopCenter)
+                    .offset { IntOffset(x = 0, y = topBarOffsetY.roundToInt()) }
+                    .background(AppColors.primary)
+            )
+            Box(
+                modifier = Modifier
+                    .height(fabBoxHeightDp)
+                    .align(Alignment.BottomCenter)
+                    .offset { IntOffset(x = 0, y = fabOffsetY.roundToInt()) }
+            ) {
+                HomeFloatingActionButton(onClickFloatingActionButton)
             }
         }
     }
 }
+
+@Composable
+private fun HomeTopBar(onClickUserProfileButton: () -> Unit, modifier: Modifier = Modifier) =
+    Box(modifier = modifier, contentAlignment = Alignment.BottomCenter) {
+        TopAppBar(
+            title = { Text("Feed") },
+            actions = {
+                BottomBarIconButton(onClickUserProfileButton, Icons.Rounded.Settings)
+            }
+        )
+    }
 
 @Composable
 private fun UploadingNotifierItem(activityStorageCount: Int) = Column(
@@ -93,34 +151,31 @@ private fun PreviewUploadingNotifierItem() {
 }
 
 @Composable
-private fun HomeFloatingActionButton(onClick: () -> Unit) = FloatingActionButton(
-    onClick = onClick
-) {
-    Icon(
-        imageVector = Icons.Rounded.Add,
-        contentDescription = "Floating action button on bottom bar"
-    )
-}
+private fun HomeFloatingActionButton(onClick: () -> Unit, modifier: Modifier = Modifier) =
+    FloatingActionButton(onClick = onClick, modifier = modifier) {
+        Icon(
+            imageVector = Icons.Rounded.Add,
+            contentDescription = "Floating action button on bottom bar"
+        )
+    }
 
 @Composable
 private fun HomeBottomBar(
     onClickUserProfileButton: () -> Unit
-) = BottomAppBar(
-    elevation = 2.dp
-) {
-    Spacer(modifier = Modifier.weight(weight = 1f, fill = true))
+) = BottomAppBar(elevation = 4.dp) {
+    BottomBarIconButton(onClickUserProfileButton, Icons.Rounded.Settings)
+    BottomBarIconButton(onClickUserProfileButton, Icons.Rounded.Settings)
     BottomBarIconButton(onClickUserProfileButton, Icons.Rounded.Settings)
 }
 
 @Composable
-private fun BottomBarIconButton(onClick: () -> Unit, iconImageVector: ImageVector) = IconButton(
-    onClick = onClick,
-) {
-    Icon(
-        imageVector = iconImageVector,
-        contentDescription = "User profile icon button on bottom bar"
-    )
-}
+private fun BottomBarIconButton(onClick: () -> Unit, iconImageVector: ImageVector) =
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = iconImageVector,
+            contentDescription = "User profile icon button on bottom bar"
+        )
+    }
 
 @Preview
 @Composable
