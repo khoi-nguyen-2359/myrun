e461a8237ac953ce3043a89299aec1e9625bdc85
Khoi Nguyen <akhoi90@gmail.com>
2021-08-21 11:13:08 +0700

    [MR-80] Replace save icon by Save text

--- features/base/src/main/java/akio/apps/myrun/feature/base/ui/AppBarIconButton.kt
+++ features/base/src/main/java/akio/apps/myrun/feature/base/ui/AppBarIconButton.kt
@@ -2,20 +2,35 @@ package akio.apps.myrun.feature.base.ui
 
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun AppBarIconButton(
     iconImageVector: ImageVector,
     contentDescription: String? = null,
     onClick: () -> Unit,
-) =
-    IconButton(onClick = onClick) {
-        Icon(
-            tint = Color.White,
-            imageVector = iconImageVector,
-            contentDescription = contentDescription
-        )
-    }
+) = IconButton(onClick = onClick) {
+    Icon(
+        tint = Color.White,
+        imageVector = iconImageVector,
+        contentDescription = contentDescription
+    )
+}
+
+@Composable
+fun AppBarTextButton(
+    text: String,
+    onClick: () -> Unit,
+) = IconButton(onClick = onClick) {
+    Text(
+        fontWeight = FontWeight.SemiBold,
+        text = text.uppercase(),
+        color = Color.White,
+        fontSize = 14.sp
+    )
+}
