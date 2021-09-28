02ebadd67ec354389e4ae92af3576d1fddc99556
Khoi Nguyen <akhoi90@gmail.com>
2021-09-28 22:39:25 +0700

    MR-103 Add api for place search

--- data/location/api/src/main/java/akio/apps/myrun/data/location/api/PlaceDataSource.kt
+++ data/location/api/src/main/java/akio/apps/myrun/data/location/api/PlaceDataSource.kt
@@ -2,10 +2,12 @@ package akio.apps.myrun.data.location.api
 
 import akio.apps.myrun.data.location.api.model.PlaceAddressComponent
 import akio.apps.myrun.data.location.api.model.PlaceDetails
+import akio.apps.myrun.data.location.api.model.PlaceSuggestion
 
 interface PlaceDataSource {
     suspend fun getCurrentPlace(): PlaceDetails?
     suspend fun getAddressFromLocation(lat: Double, lng: Double): List<PlaceAddressComponent>
     suspend fun getRecentPlaceAddressFromLocation(lat: Double, lng: Double):
         List<PlaceAddressComponent>
+    suspend fun getPlaceSuggestions(query: String): List<PlaceSuggestion>
 }
