32d5813c24cef8a4832c28e35a7e4f84f2213651
Khoi Nguyen <akhoi90@gmail.com>
2021-12-23 23:12:15 +0700

    MR-122 Resolve the start place address when starting to upload activity

--- domain/src/main/java/akio/apps/myrun/domain/user/UpdateUserRecentPlaceUsecase.kt
+++ domain/src/main/java/akio/apps/myrun/domain/user/UpdateUserRecentPlaceUsecase.kt
@@ -3,8 +3,6 @@ package akio.apps.myrun.domain.user
 import akio.apps.myrun.data.authentication.api.UserAuthenticationState
 import akio.apps.myrun.data.location.api.LocationDataSource
 import akio.apps.myrun.data.location.api.PlaceDataSource
-import akio.apps.myrun.data.tracking.api.RouteTrackingState
-import akio.apps.myrun.data.tracking.api.model.RouteTrackingStatus
 import akio.apps.myrun.data.user.api.UserRecentPlaceRepository
 import java.io.IOException
 import javax.inject.Inject
@@ -14,10 +12,9 @@ class UpdateUserRecentPlaceUsecase @Inject constructor(
     private val placeDataSource: PlaceDataSource,
     private val userAuthenticationState: UserAuthenticationState,
     private val userRecentPlaceRepository: UserRecentPlaceRepository,
-    private val routeTrackingState: RouteTrackingState,
     private val placeIdentifierConverter: PlaceIdentifierConverter,
 ) {
-    suspend operator fun invoke(): Result {
+    suspend fun updateUserRecentPlace(): Result {
         val userId = userAuthenticationState.getUserAccountId()
             ?: return Result.InvalidUser
         val lastLocation = locationDataSource.getLastLocation()
@@ -32,10 +29,6 @@ class UpdateUserRecentPlaceUsecase @Inject constructor(
             val placeIdentifier =
                 placeIdentifierConverter.fromAddressNameList(sortedAddressTexts.map { it.name })
             userRecentPlaceRepository.saveRecentPlace(userId, placeIdentifier)
-
-            if (routeTrackingState.getTrackingStatus() != RouteTrackingStatus.STOPPED) {
-                routeTrackingState.setPlaceIdentifier(placeIdentifier)
-            }
         } catch (ex: IOException) {
             return Result.IOFailure
         }
