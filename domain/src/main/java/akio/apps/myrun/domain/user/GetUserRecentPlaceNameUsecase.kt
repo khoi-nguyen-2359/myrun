429579ffbd3144e4e4c0b741563499cc8181089f
Khoi Nguyen <akhoi90@gmail.com>
2022-08-04 23:29:38 +0700

    Fix blank space is showing for empty user's recent place in user stats

--- domain/src/main/java/akio/apps/myrun/domain/user/GetUserRecentPlaceNameUsecase.kt
+++ domain/src/main/java/akio/apps/myrun/domain/user/GetUserRecentPlaceNameUsecase.kt
@@ -1,11 +1,13 @@
 package akio.apps.myrun.domain.user
 
 import akio.apps.myrun.data.authentication.api.UserAuthenticationState
+import akio.apps.myrun.data.common.Resource
 import akio.apps.myrun.data.user.api.PlaceIdentifier
 import akio.apps.myrun.data.user.api.UserRecentPlaceRepository
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class GetUserRecentPlaceNameUsecase @Inject constructor(
     private val userRecentPlaceRepository: UserRecentPlaceRepository,
@@ -14,8 +16,10 @@ class GetUserRecentPlaceNameUsecase @Inject constructor(
 ) {
     fun getUserRecentPlaceNameFlow(): Flow<PlaceIdentifier?> {
         val userId = userAuthenticationState.requireUserAccountId()
-        return userRecentPlaceRepository.getRecentPlaceIdentifierFlow(userId).map {
-            placeNameSelector.select(it.data, it.data)
-        }
+        return userRecentPlaceRepository.getRecentPlaceIdentifierFlow(userId)
+            .onStart { emit(Resource.Success(null)) }
+            .map {
+                placeNameSelector.select(it.data, it.data)
+            }
     }
 }
