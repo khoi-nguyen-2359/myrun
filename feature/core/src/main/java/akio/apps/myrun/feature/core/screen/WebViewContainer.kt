74541c44a423478e8d42775738e58eebb3aa7c5e
Khoi Nguyen <akhoi90@gmail.com>
2022-09-16 22:06:04 +0700

    MR-145 Show loading bar on top of the screen

--- feature/core/src/main/java/akio/apps/myrun/feature/core/screen/WebViewContainer.kt
+++ feature/core/src/main/java/akio/apps/myrun/feature/core/screen/WebViewContainer.kt
@@ -5,13 +5,20 @@ import akio.apps.myrun.feature.core.navigation.HomeNavDestination
 import akio.apps.myrun.feature.core.ui.AppBarArrowBackButton
 import akio.apps.myrun.feature.core.ui.AppTheme
 import akio.apps.myrun.feature.core.ui.StatusBarSpacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import com.google.accompanist.web.WebView
@@ -33,10 +40,18 @@ fun WebViewContainer(navController: NavHostController, navEntry: NavBackStackEnt
             }
         )
 
-        WebView(
-            state = rememberWebViewState(url),
-            modifier = Modifier.fillMaxSize(),
-            captureBackPresses = true
-        )
+        val webViewState = rememberWebViewState(url)
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            WebView(
+                state = webViewState,
+                modifier = Modifier.fillMaxSize(),
+                captureBackPresses = true
+            )
+
+            if (webViewState.isLoading) {
+                LinearProgressIndicator(modifier = Modifier.fillMaxWidth().height(2.dp))
+            }
+        }
     }
 }
