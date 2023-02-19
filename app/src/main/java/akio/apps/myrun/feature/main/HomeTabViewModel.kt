99b11e8f67838f9bd680aac4db4ef0ee09e2ecf4
Khoi Nguyen <akhoi90@gmail.com>
2023-02-19 22:32:10 +0800

    remove BaseViewModel

--- app/src/main/java/akio/apps/myrun/feature/main/HomeTabViewModel.kt
+++ app/src/main/java/akio/apps/myrun/feature/main/HomeTabViewModel.kt
@@ -2,11 +2,6 @@ package akio.apps.myrun.feature.main
 
 import akio.apps.myrun.data.tracking.api.RouteTrackingState
 import akio.apps.myrun.data.tracking.api.model.RouteTrackingStatus
-import akio.apps.myrun.feature.core.BaseViewModel
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
